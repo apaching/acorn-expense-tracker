31fa1f747166ed0c12c157d61f7d7cce7669b3e9
Yves Domingo <yvesrafaeladomingo@gmail.com>
2025-06-06 21:22:19 +0800

    fix: settings page

--- src/app/(acorn)/dashboard/settings/page.tsx
+++ src/app/(acorn)/dashboard/settings/page.tsx
@@ -1,3 +1,15 @@
+import { HardHat } from "lucide-react";
+
 export default function Settings() {
-  return <div>Settings Page </div>;
+  return (
+    <div className="bg-gray flex h-full w-full flex-col items-center justify-center">
+      <div className="flex items-center gap-3">
+        <HardHat className="h-6 w-6 text-primary" />
+        <h1 className="text-center text-xl font-semibold">
+          This page is under construction.
+        </h1>
+      </div>
+      <p className="mt-2 text-sm text-gray-500">Please check back later.</p>
+    </div>
+  );
 }
