017969bbd885eabca58126e55b89173711fb234d
Yves Domingo <yvesrafaeladomingo@gmail.com>
2025-06-06 22:14:53 +0800

    fix build error

--- src/app/(acorn)/dashboard/transactions/page.tsx
+++ src/app/(acorn)/dashboard/transactions/page.tsx
@@ -1,13 +1,6 @@
-import { createClient } from "@/utils/supabase/server";
 import { TransactionHistory } from "@/components/all-transactions";
 
 export default async function Transactions() {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   return (
     <div className="flex h-screen flex-col">
       <div className="flex-1 overflow-hidden px-10 py-20">
