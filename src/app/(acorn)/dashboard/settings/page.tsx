0fd2972b3071eabb0781c0305bc09bc638204ff2
Yves Domingo <yvesrafaeladomingo@gmail.com>
2025-06-06 21:22:59 +0800

    fix: changed settings logo

--- src/app/(acorn)/dashboard/settings/page.tsx
+++ src/app/(acorn)/dashboard/settings/page.tsx
@@ -1,10 +1,10 @@
-import { HardHat } from "lucide-react";
+import { Construction } from "lucide-react";
 
 export default function Settings() {
   return (
     <div className="bg-gray flex h-full w-full flex-col items-center justify-center">
       <div className="flex items-center gap-3">
-        <HardHat className="h-6 w-6 text-primary" />
+        <Construction className="h-6 w-6 text-primary" />
         <h1 className="text-center text-xl font-semibold">
           This page is under construction.
         </h1>
